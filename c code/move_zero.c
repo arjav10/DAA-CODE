eb23c9231ff8ef989ac212a6977c2783021ed1f7
arjav10 <arjavj514@gmail.com>
2025-08-22 11:43:31 +0530

    Create move_zero.c

---
+++ c code/move_zero.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void moveZeroes(int arr[], int n) {
+    int j = 0;  
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
+        }
+    }
+}
+int main() {
+    int arr[] = {0, 1, 0, 3, 12};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    moveZeroes(arr, n);
+    printf("Array after moving zeroes: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
